c666c870678d10c82f7886560ff58508c192f801
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-09 01:24:53 +0200

    complete exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const StatisticLine = ({text, value}) => {
+  if (text === "positive") {
+    return <p>{text} {value} %</p>
+  }
+
+  return <p>{text} {value}</p>
+}
+
 const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad
   const average = ((good - bad) / all) || 0
@@ -11,12 +19,12 @@ const Statistics = ({good, neutral, bad}) => {
 
   return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
     </div>
   )
 }
