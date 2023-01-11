0b5df6bde4e4cf314015ba888bff77b14e654efc
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-11 20:04:12 +0200

    complete exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,40 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <p>
-      <b>total of {total} exercises</b>
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
@@ -82,7 +46,6 @@ const App = () => {
     },
   ];
 
-  // return <Course course={course} />;
   return (
     <>
       <h1>Web development curriculum</h1>
