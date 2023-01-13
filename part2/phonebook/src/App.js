3a0ddc15f47afc3eeadf4f051a639b23a1d38678
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-13 19:22:08 +0200

    complete exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterQuery, setFilterQuery] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -11,6 +17,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const handleFilterQueryChange = (event) => {
+    setFilterQuery(event.target.value);
   };
 
   const isAlreadyAdded = (newPerson) => {
@@ -49,9 +59,24 @@ const App = () => {
     setNewNumber("");
   };
 
+  const displayedPersons = () => {
+    if (!filterQuery) {
+      return persons;
+    }
+
+    return persons.filter((person) =>
+      person.name.toLowerCase().includes(filterQuery.toLowerCase())
+    );
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filterQuery} onChange={handleFilterQueryChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -65,7 +90,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
+        {displayedPersons().map((person) => (
           <p key={person.name}>
             {person.name} {person.number}
           </p>
