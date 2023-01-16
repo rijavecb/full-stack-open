01953062b38f3d119bf6ecd19ba0cb2759c1fbb2
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-16 10:59:01 +0200

    complete exercise 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,9 +1,24 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Country = ({ country, isDisplayed }) => {
   const [isShown, setIsShown] = useState(isDisplayed);
+  const [weatherInfo, setWeatherInfo] = useState({});
+
   useEffect(() => {
     setIsShown(isDisplayed);
+    const apiQuery = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]},${country.cca2}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`;
+
+    axios.get(apiQuery).then((response) => {
+      setWeatherInfo(() => {
+        return {
+          temp: response.data.main.temp,
+          icon: response.data.weather[0].icon,
+          description: response.data.weather[0].description,
+          wind: response.data.wind.speed,
+        };
+      });
+    });
   }, [isDisplayed]);
 
   if (!isShown) {
@@ -28,6 +43,13 @@ const Country = ({ country, isDisplayed }) => {
         ))}
       </ul>
       <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+      <h3>Weather in {country.capital[0]}</h3>
+      <p>temperature {weatherInfo.temp} Celsius</p>
+      <img
+        src={`http://openweathermap.org/img/wn/${weatherInfo.icon}.png`}
+        alt={weatherInfo.description}
+      />
+      <p>wind {weatherInfo.wind} m/s</p>
     </div>
   );
 };
